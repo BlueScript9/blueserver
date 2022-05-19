7772866ff5edce66d48abe5a65be83a5c3c6ff1a
BlueScript9 <47090464+BlueScript9@users.noreply.github.com>
2022-05-19 09:40:02 -0500

    Update client.js

--- client.js
+++ client.js
@@ -1,11 +1,11 @@
-document.write('<!DOCTYPE html>
+document.write("<!DOCTYPE html>
 <html>
                <head>
                <title>bEditor</title>
                </head>
               
-               <body style="text-color: white">
-               <textarea style="background-color: black" placeholder="Enter HTML code here...."></textarea>
+               <body style='text-color: white'>
+               <textarea style='background-color: black' placeholder='Enter HTML code here....'></textarea>
                <iframe></iframe>
                </body>
-               </html>');               
+               </html>");               
